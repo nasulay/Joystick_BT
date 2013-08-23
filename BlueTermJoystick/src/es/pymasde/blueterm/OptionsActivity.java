34bc71ab9e2868cf32ca458684452368c81bda22
nasulay <nikhil_sulay@hotmail.com>
2013-08-23 17:38:07 -0400

    Seek Bar in development

---
+++ BlueTermJoystick/src/es/pymasde/blueterm/OptionsActivity.java
@@ -0,0 +1,21 @@
+/***************************************
+ * 
+ * Android Bluetooth Dual Joystick
+ * yus - projectproto.blogspot.com
+ * October 2012
+ *  
+ ***************************************/
+
+package es.pymasde.blueterm;
+
+import android.preference.PreferenceActivity;
+import android.os.Bundle;
+
+public class OptionsActivity extends PreferenceActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+	}
+	
+}
